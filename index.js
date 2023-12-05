e016d29639ad102dfd7ce1eeb98320877cd7e648
yosinbek08 <152950684+yosinbek08@users.noreply.github.com>
2023-12-05 10:47:18 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function Hello(params) {
-    let inp =document.getElementById("inp").value
-    document.getElementById("text").style.borderRadius=inp+"px"
-}
+function Hello() {
+ let a= document.getElementById ("raqam").value
+ let b= document.getElementById("text")
+
+    switch (a) {
+        case "+998":
+            b.innerHTML="Uzbekistan"
+            break;
+        case "+7":
+            b.innerHTML="Russia"
+            break;
+        case "+55":
+            b.innerHTML="Korea"
+            break;
+        case "+996":
+            b.innerHTML="Kazakistan"
+            break;
+        case "+1":
+            b.innerHTML="America"
+            break;
+            
+            default:
+                console.log("xato");
+                break;
+            }
+        }
